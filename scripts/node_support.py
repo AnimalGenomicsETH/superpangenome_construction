3aa2421857418535b4e66e5514768cdf4ab4dfb8
danangcrysnanto <danangcrysnanto@gmail.com>
2022-03-11 11:04:45 +0100

    Fix bug: not considering ref nodes in alt allele

--- scripts/node_support.py
+++ scripts/node_support.py
@@ -23,6 +23,16 @@ def node_coverage(input_file):
                 nodesup[comp] += 1
     return nodesup
 
+#coverage based on the edges 
+def edges_coverage(input_file):
+    edgesup=defaultdict(int)
+
+    with open(input_file) as infile:
+        for line in infile:
+            allnode=line.strip().split("\t")[5].replace(">","<").split("<")
+            for ind,comp in enumerate(allnode[:-1]):
+                nodepair=f"{allnode[ind]}_{allnode[ind+1]}"
+                edgesup[nodepair] += 1
 
 def allele_checker(allele,nodesup):
     sup = []
@@ -32,15 +42,37 @@ def allele_checker(allele,nodesup):
     return min(sup)
 
 
+def edge_checker(allele,edgesup):
+    sup = []
+    for ind,comp in enumerate(allele[:-1]):
+        sup.append(nodesup.get(f"{allele[ind]}_{allele[ind+1]}",0))
+    return min(sup)
+
 if __name__ == "__main__":
     nodesup = node_coverage(sys.argv[1])
 
+   #with open(sys.argv[2]) as alignfile:
+        #for line in alignfile:
+            #if not line.startswith("#"):
+                #all_allele = re.findall(r"AT=([^;]+);",line)[0].split(",")
+                #allele_sup = []
+                #for allele in all_allele:
+                #    allnode=allele.replace(">","<").split("<")
+                #    allele_sup.append(allele_checker(allnode,nodesup))
+                #print(line.strip(),",".join(str(x) for x in allele_sup),allele_sup[0],min(allele_sup[1:]),max(allele_sup[1:]))
+                
     with open(sys.argv[2]) as alignfile:
         for line in alignfile:
             if not line.startswith("#"):
                 all_allele = re.findall(r"AT=([^;]+);",line)[0].split(",")
-                allele_sup = []
-                for allele in all_allele:
+                ref_allele = all_allele[0].replace(">","<").split("<")
+                ref_sup=allele_checker(ref_allele,nodesup)
+                allele_sup = [ref_sup]
+                for allele in all_allele[1:]:
                     allnode=allele.replace(">","<").split("<")
-                    allele_sup.append(allele_checker(allnode,nodesup))
+                    altnode=[x for x in allnode if x not in ref_allele]
+                    if not altnode:
+                        allele_sup.append(allele_checker(allnode,nodesup))
+                    else:
+                        allele_sup.append(allele_checker(altnode,nodesup))
                 print(line.strip(),",".join(str(x) for x in allele_sup),allele_sup[0],min(allele_sup[1:]),max(allele_sup[1:]))
