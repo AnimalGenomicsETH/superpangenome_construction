3972f564ed72d67883fffb76a1401698d3508442
danangcrysnanto <danangcrysnanto@gmail.com>
2022-05-06 12:22:21 +0200

    Fix assembly ordering

--- scripts/combine_annot_allele.py
+++ scripts/combine_annot_allele.py
@@ -6,6 +6,7 @@ Merged annotation of jasmine SV with the allelic length from graphs
 """
 
 import sys
+import re
 import argparse
 from collections import defaultdict
 
@@ -26,9 +27,18 @@ if __name__ == "__main__":
     
     for ind,db in enumerate(args.database):
         with open(db) as infile:
+            if ind == 0:
+                anim_order=next(infile).strip().split()[3:]
+                anim_order=[re.findall(r"[A-Za-z]+",x)[0] for x in anim_order]
+                anim_id = anim_order
+            else:
+                anim_id = next(infile).strip().split()[3:]
+                anim_id=[re.findall(r"[A-Za-z]+",x)[0] for x in anim_order]
             for line in infile:
                 varid=line.strip().split()[2]
-                vardict[ind][varid]=line.strip().split()[3:]
+                vardict[ind][varid]={x:y for x,y in zip(anim_id,line.strip().split()[3:])}
+    
+    print("chromo","startpos","mergeid","mergevector","geneid","pathid",*anim_id)
 
     with open(args.annot) as infile:
         for line in infile:
@@ -42,9 +52,12 @@ if __name__ == "__main__":
                 # if int(prog) in [0,1,2]:
                 # somehow there is mixed up in minigraph id
                 try:
-                   print(*tok,varid,*vardict[int(prog)][varid])
+                   all_allele=vardict[int(prog)][varid]
+                   # print(all_allele)
                 except:
                     continue
+                allele_ordered = [ all_allele[x] for x in anim_order  ]
+                print(*tok,varid,*allele_ordered)
 
                 # print(tok,prog,varid)
 
