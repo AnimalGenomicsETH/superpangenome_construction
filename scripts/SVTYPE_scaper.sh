ecd9ea345f35efa9f83d3c12370b781d64ba7a73
ASLeonard <tkom343@gmail.com>
2022-09-08 17:19:59 +0200

    update jasmine and bcftools for simple structure

--- scripts/SVTYPE_scaper.sh
+++ scripts/SVTYPE_scaper.sh
@@ -1,17 +1,10 @@
 echo "pangenome chromosome deletions insertions other"
-for p in pggb cactus
+for p in pggb cactus minigraph assembly
 do
   for c in {1..29}
   do
     echo -n "${p} ${c} "
-    echo $(bcftools stats /cluster/work/pausch/to_share/sv_analysis/final_data/snarl/snarl_new/${p}/${p}_${c}_snarl_norm.vcf |\
+    echo $(bcftools stats ${p}/${c}.vcf |\
     awk '{if($5~/others/){O+=$6}else{if($1=="IDD"){if($3~/-/){D+=$4}else{I+=$4}}}} END {print D,I,O}')
   done
-done
-
-for c in {1..29}
-do
-  echo -n "minigraph ${c} "
-  echo $(bcftools stats /cluster/work/pausch/to_share/sv_analysis/final_data/snarl/snarl_new/minigraph_gt/minigraph_${c}_snarlgt_norm.vcf |\
-  awk '{if($5~/others/){O+=$6}else{if($1=="IDD"){if($3~/-/){D+=$4}else{I+=$4}}}} END {print D,I,O}')
 done
