45f1066dac8ac7ba62a37056e3ab30a59b48e7fa
ASLeonard <tkom343@gmail.com>
2022-09-30 13:35:33 +0200

    download and prep assemblies

---
+++ snakepit/prep_assemblies.sh
@@ -0,0 +1,19 @@
+## LOCAL ASSEMBLIES
+for ID in GAU,GxP.hap1 PIE,GxP.hap2 NEL,NxB.hap1 BSW,NxB.hap2 OBV,OxO.hap2
+do
+  seqtk subseq /nfs/nas12.ethz.ch/fs1201/green_groups_tg_public/data/assemblies/CCS/${ID#*,}.hifiasm.fasta <(echo {1..29} | tr ' ' '\n') > ${ID%,*}.fasta
+done
+
+##LOCAL COPY OF REFERENCE
+seqtk subseq /cluster/work/pausch/inputs/ref/BTA/UCD1.2/ARS-UCD1.2_Btau5.0.1Y.fa <(echo {1..29} | tr ' ' '\n') > ref.fasta
+
+
+## OTHER ASSEMBLIES AVAIBLE VIA NCBI
+for ID in BRA,GCF_003369695.1 ANG,GCA_003369685.2 SIM,GCA_018282465.1 HIG,GCA_009493655.1 YAK,GCA_009493645.1 BIS,GCA_018282365.1
+do
+  curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v1/genome/accession/${ID#*,}/download?filename=${ID#*,}.zip" -H "Accept: application/zip"
+  unzip -o ${ID#*,}.zip && rm ${ID#*,}.zip
+  for i in {1..29}; do cat ncbi_dataset/data/${ID#*,}/chr${i}.fna | sed "s/>.*/>${i}/g" | seqtk seq >> ${ID%,*}.fasta; done
+done
+
+rm -rf ncbi_dataset
